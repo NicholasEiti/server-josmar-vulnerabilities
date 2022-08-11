57c8b9a4d29942eec83db0a907ac602315f7f164
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-11 14:39:13 -0300

    Create library.php

---
+++ library.php
@@ -0,0 +1,28 @@
+<?php
+
+define('__ROOT__', dirname(__FILE__));
+
+set_error_handler(function ($err_no, $err_str, $err_file, $err_line) {
+    header('Content-Type: application/json');
+    echo json_encode([
+        'error' => [
+            'level' => $err_no,
+            'msg' => $err_str,
+            'filename' => $err_file,
+            'line' => $err_line
+        ]
+    ]);
+    exit;
+});
+
+require_once __ROOT__ . "/db.class.php";
+
+$configs = (object) parse_ini_file(__ROOT__ . "/configurations.ini");
+
+$GLOBAL_PDO = DBRequest::startPDO(
+    $configs->db_name,
+    $configs->db_host,
+    $configs->db_charset,
+    $configs->db_login,
+    $configs->db_password
+);
