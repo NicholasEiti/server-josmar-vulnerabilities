9a3d975a05ce86d6dc4e9acd76d58f0f2f94b129
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-12 00:17:28 -0300

    Create sessions table

--- docker/mysql/scripts/init.sql
+++ docker/mysql/scripts/init.sql
@@ -5,16 +5,16 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `Drawers`;
-CREATE TABLE `Drawers` (
+DROP TABLE IF EXISTS `drawers`;
+CREATE TABLE `drawers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-DROP TABLE IF EXISTS `Keys`;
-CREATE TABLE `Keys` (
+DROP TABLE IF EXISTS `keys`;
+CREATE TABLE `keys` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `drawer` int NOT NULL,
@@ -22,8 +22,8 @@ CREATE TABLE `Keys` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-DROP TABLE IF EXISTS `Requests`;
-CREATE TABLE `Requests` (
+DROP TABLE IF EXISTS `requests`;
+CREATE TABLE `requests` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user` int NOT NULL,
   `key` int NOT NULL,
@@ -35,12 +35,19 @@ CREATE TABLE `Requests` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-DROP TABLE IF EXISTS `Users`;
-CREATE TABLE `Users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `level` int NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `access` timestamp NOT NULL,
+  `data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
