7a115cb990d468be0f007f1c64ab9f3963586746
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-12 00:17:04 -0300

    Add return types in API class

--- api.class.php
+++ api.class.php
@@ -1,13 +1,15 @@
 <?php
 
 class API {
-    static private function _send($data) {
+    static private function _send($data): never
+    {
         header('Content-Type: application/json');
         echo json_encode($data);
         exit;
     }
 
-    static function send_error($error_info) {
+    static function send_error($error_info): never
+    {
         static::_send([
             'error' => $error_info,
             'status' => 'error'
