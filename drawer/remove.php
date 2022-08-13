49705d0f866f390968eb075b4c9efac06be54594
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-13 13:55:18 -0300

    Delete keys on delete drawer, Fix #6

--- drawer/remove.php
+++ drawer/remove.php
@@ -15,6 +15,15 @@ $drawer = DBRequest::searchById('drawers', $drawer_id);
 if ($drawer === false)
     API::send_error('Drawer not found.');
 
+$keys = DBRequest::search('keys', 'WHERE drawer = ?', [$drawer_id]);
+
+if ($keys !== false) {
+    $key_ids = array_map(fn ($key) => $key['id'], $keys);
+
+    if (!DBRequest::deleteById('keys', $key_ids))
+        API::send_error('Something wrong on remove keys, try again.');
+}
+
 if (!DBRequest::deleteById('drawers', $drawer_id))
     API::send_error('Something wrong on remove drawer, try again.');
 
