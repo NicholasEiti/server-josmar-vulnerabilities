61f7b226222024006e2f6da60303c36b36e7a125
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-12 22:35:30 -0300

    Simplify /drawer/create

--- drawer/create.php
+++ drawer/create.php
@@ -6,44 +6,12 @@
 
 require_once "../library.php";
 
-/**
- * Get regular name
- * 'test' => test
- * 'teste_asd' => teste_asd
- * '123' => 123
- * 'asd123asd' => asd123asd
- * '' => False
- * 123 => 123
- */
-function getRegularName(string $name, $lenght=50): false|string
-{
-    $name_len = strlen($name);
+$drawer_name = Params::getParams('name', min_length: 5, max_length: 10);
 
-    if ($name_len >= $lenght or $name_len <= 0)
-        return False;
+if (DBRequest::search('drawers', 'WHERE name = ?', [$drawer_name]) !== False)
+    API::send_error('Name is already in use.');
 
-    return trim($name);
-}
+if (!DBRequest::insert('drawers', [ 'name' => $drawer_name ]))
+    API::send_error('Something wrong on create drawer, try again.');
 
-$drawer_name = getRegularName($_GET['name']);
-
-if ($drawer_name === False) {
-    API::send_error([
-        'msg' => 'Unexpected name'
-    ]);
-}
-
-if (DBRequest::search('drawers', 'WHERE name = ?', [$drawer_name]) !== False) {
-    API::send_error([
-        'msg' => 'Name is already in use'
-    ]);
-}
-
-DBRequest::insert('drawers', [
-    'name' => $drawer_name
-]);
-
-API::send_success([
-    'msg' => 'Drawer created.',
-    'drawer_id' => DBRequest::get_last_id('drawers')
-]);
+API::send_success('Drawer created.', [ 'id' => DBRequest::get_last_id('drawers') ]);
