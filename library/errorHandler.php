4df989fd605f8d1a83e99ccb6b6cdf5585050822
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-13 14:57:29 -0300

    Create /library/errorHandler.php

---
+++ library/errorHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+function errorHandler($err_no, $err_str, $err_file, $err_line) {
+    global $configs;
+
+    if ($configs->on_development)
+        API::send_error_without_code($err_str, [
+            'level' => $err_no,
+            'filename' => $err_file,
+            'line' => $err_line
+        ]);
+}
+
+set_error_handler('errorHandler', E_ALL | E_STRICT);
