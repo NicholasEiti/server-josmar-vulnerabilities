ae5743ef2d3ce15e4435d65dab2764f67e9a3acc
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-13 21:22:10 -0300

    Create /user/create #4

---
+++ user/create.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Criar Usuário
+ * /user/create
+ */
+
+require_once "../library/library.php";
+
+define('EMAIL_PATTERN', "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/");
+$ENUM_USER_LEVELS    = [ 'collaborator' => 5, 'admin' => 15 ];
+
+API::requestMethodMustBe('GET');
+
+$user_name      = Params::getParam('name', min_length: 5, max_length: 20);
+$user_password  = Params::getParam('password', min_length: 5, max_length: 20);
+$user_email     = Params::getRegexParam('email', EMAIL_PATTERN);
+$user_level     = Params::getEnumParam('level', $ENUM_USER_LEVELS);
+
+if (UserDB::count('WHERE name = ?', [$user_name]) !== 0)
+    API::send_error('user_name_in_use');
+
+if (UserDB::count('WHERE email = ?', [$user_email]) !== 0)
+    API::send_error('user_email_in_use');
+
+if (!UserDB::insert([
+    'name'      => $user_name,
+    'password'  => $user_password,
+    'email'     => $user_email,
+    'level'     => $user_level
+]))
+    API::send_error('user_error_on_create');
+
+API::send_success('user_created', [ 'id' => UserDB::get_last_id() ]);
