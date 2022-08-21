8ed9ac49840efaeaa1f432927d4692b25fdf3e98
Mocno <gustavo.correa.mogi@gmail.com>
2022-08-21 12:34:11 -0300

    Add developments values to db

--- docker/mysql/scripts/init.sql
+++ docker/mysql/scripts/init.sql
@@ -12,6 +12,9 @@ CREATE TABLE `drawers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `drawers` (`id`, `name`) VALUES
+(1,	'Main drawer'),
+(2,	'Secondary drawer');
 
 DROP TABLE IF EXISTS `keys`;
 CREATE TABLE `keys` (
@@ -21,6 +24,11 @@ CREATE TABLE `keys` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `keys` (`id`, `name`, `drawer`) VALUES
+(1,	'Room-302',	1),
+(2,	'Room-301',	1),
+(3,	'Room-303',	1),
+(4,	'Room-501',	2);
 
 DROP TABLE IF EXISTS `requests`;
 CREATE TABLE `requests` (
@@ -35,20 +43,21 @@ CREATE TABLE `requests` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+INSERT INTO `requests` (`id`, `user`, `key`, `status`, `date_start`, `date_expected_start`, `date_end`, `date_expected_end`) VALUES
+(1,	1,	1,	1,	NULL,	'2022-09-10 23:59:59',	NULL,	'2022-09-20 23:59:59'),
+(2,	2,	2,	1,	NULL,	'2022-09-12 12:00:00',	NULL,	'2022-09-12 14:00:00');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `email` varchar(50) NOT NULL,
   `level` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `sessions`;
-CREATE TABLE `sessions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `access` timestamp NOT NULL,
-  `data` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `level`) VALUES
+(1,	'Renan',	'$2y$12$yj9DXhcGipsz3WOVmONIT.Q7apNNnBUVyO/V1faj2a84fQguhAovW',	'renanpraga@gmail.com',	5), -- password_of_renan
+(2,	'Bruno',	'$2y$12$fucWqL46jyUjWaiWV4rGJOPJ3NngEqGUKgIyeiCzXBnxoeQmFW9zC',	'brunorocha@gmail.com',	5), -- password_of_bruno
+(3,	'admin',	'$2y$12$i6yB/QqcANQf6nZ.HOOBMuG1GYYJugBkTxQc8auWc/Z0JOoVcoaMi',	'admin@gmail.com',	15); -- password_of_admin
