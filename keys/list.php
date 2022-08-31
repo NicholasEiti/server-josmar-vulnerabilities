43314f4e7481ba6915c288da69d6db435b84c254
mocno <gustavo.correa.mogi@gmail.com>
2022-08-31 18:25:10 +0000

    Create `/keys/` #11

---
+++ keys/list.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "../library/library.php";
+
+$access = new Access;
+
+$access->loggedPage();
+
+Fabric::generateHead("Listagem dos armários", [
+    "/static/list-block-api.js"
+], [
+    "/static/main-logged-in.css"
+]);
+
+Fabric::generateLoggedInStart($access);
+Fabric::generateListBlock('key');
+Fabric::generateFooter();
