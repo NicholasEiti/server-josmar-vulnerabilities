ae4ec43d21e9cad7fdbd6c3f9c39d96152fbad78
Mocno <gustavo.correa.mogi@gmail.com>
2022-09-03 16:29:09 -0300

    Put title #13

--- static/list-block-api.js
+++ static/list-block-api.js
@@ -19,12 +19,21 @@ class ListBlockElement extends HTMLElement {
         request: 'Não há pedidos cadastrados'
     }
 
+    getTitle = {
+        drawer: (count) => `Foram encontrados ${count} armário` + (count != 1 ? 's' : ''),
+        key: (count) => `Foram encontrados ${count} chave` + (count != 1 ? 's' : ''),
+        request: (count) => `Foram encontrados ${count} pedido` + (count != 1 ? 's' : '')
+    }
+
     DEFAULT_QUANT = 10
     DEFAULT_PAGE = 1
 
     constructor(...args) { super(...args); }
 
     generateList(tag) {
+        let containerElement = document.createElement('div');
+        containerElement.classList.add('container-block');
+
         let listElement = document.createElement('div')
         listElement.classList.add('list-block');
         let token = getToken();
@@ -33,6 +42,7 @@ class ListBlockElement extends HTMLElement {
 
         var generateItem = this.GENERATE_ITEM_FUNCS[tag];
         var urlTag = this.URL_TAGS[tag];
+        let getTitle = this.getTitle[tag]
 
         requestAPI(urlTag, {
             token,
@@ -40,6 +50,9 @@ class ListBlockElement extends HTMLElement {
             quant: this.quant
         }, function(response) {
             listElement.innerHTML = '';
+
+            let titleElement = this.generateTitle(getTitle, response.count);
+            containerElement.appendChild(titleElement);
 
             if (response.list == false) {
                 let item = this.generateNoResult(tag);
@@ -56,9 +69,11 @@ class ListBlockElement extends HTMLElement {
                     }
                 }
             }
-        });
 
-        return listElement;
+            containerElement.appendChild(listElement)
+        }.bind(this));
+
+        return containerElement;
     }
 
     generateNoResult(tag) {
@@ -74,6 +89,15 @@ class ListBlockElement extends HTMLElement {
         item.appendChild(itemText);
     
         return item;
+    }
+
+    generateTitle(getTitle, count) {
+        let titleElement = document.createElement('div');
+        titleElement.classList.add('block-title');
+
+        titleElement.textContent = getTitle(count);
+
+        return titleElement;
     }
 
     connectedCallback() {
@@ -148,12 +172,26 @@ function generateKeyItem(key) {
 }
 
 function generateRequestItem(request) {
+    request.date_expected_start = new Date(request.date_expected_start).toLocaleString("pt-br")
+    request.date_expected_end = new Date(request.date_expected_end).toLocaleString("pt-br")
+
     let item = document.createElement('div');
     item.classList.add('item-list');
 
-    let itemText = document.createElement('p');
+    let itemText = document.createElement('div');
     itemText.classList.add('item-list-text');
-    itemText.textContent = `User id: ${request.user}, Key id: ${request.key}, Status: ${request.status}, Date expected start: ${request.date_expected_start}, Date expected end: ${request.date_expected_end} `;
+
+    let itemText1 = document.createElement('span');
+    let itemText2 = document.createElement('span');
+    let itemText3 = document.createElement('span');
+
+    itemText1.classList.add('item-list-text-title');
+
+    itemText1.textContent = `Pedido da chave ${request.key_name} de ${request.user_name}`;
+    itemText2.textContent = `Entre ${request.date_expected_start} e ${request.date_expected_end}`;
+    itemText3.textContent = `Situação: ${API_REQUEST_STATUS[request.status]}`;
+
+    itemText.append(itemText1, itemText2, itemText3);
     item.appendChild(itemText);
 
     let itemIcons = document.createElement('div')
