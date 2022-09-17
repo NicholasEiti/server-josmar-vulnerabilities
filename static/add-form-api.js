3132d024eb496ccc35dcc78d264e4c6461c417bb
Mocno <gustavo.correa.mogi@gmail.com>
2022-09-17 18:29:04 -0300

    Create add-form

--- static/add-form-api.js
+++ static/add-form-api.js
@@ -1,41 +1,14 @@
-class ListBlockElement extends HTMLElement {
+class FormAddBlockElement extends HTMLElement {
     LOADING_MSG = 'Carregando...'
+
 
     constructor(...args) { super(...args); }
 
-    generateForm(tag) {}
-
-    generateNoResult(tag) {
-        let item = document.createElement('div');
-    
-        item.classList.add('item-list');
-
-        let noResultMsg = NO_RESULT_MSG[tag];
-
-        let itemText = document.createElement('p');
-        itemText.classList.add('item-list-text');
-        itemText.textContent = noResultMsg;
-        item.appendChild(itemText);
-    
-        return item;
-    }
-
-    generateTitle(tag, count, params) {
-        let titleElement = document.createElement('div');
-        titleElement.classList.add('block-title');
-
-        let getTitle = this.GET_TITLE_FN[tag]
-
-        let titleElementText = document.createElement('span');
-        titleElementText.textContent = getTitle(count, params);
-        titleElement.appendChild(titleElementText);
-
-        return titleElement;
-    }
-
     connectedCallback() {
         this.tag = this.getAttribute('tag');
-        let listElement = this.generateForm(this.tag);
-        this.appendChild(listElement);
+        // let listElement = this.generateForm(this.tag);
+        // this.appendChild(listElement);
     }
 }
+
+window.customElements.define('form-add-block', FormAddBlockElement)
