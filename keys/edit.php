6b32d6802585ca1e054a39c807dedbcc188bee38
Mocno <gustavo.correa.mogi@gmail.com>
2022-09-22 15:52:45 -0300

    Create keys files #11

---
+++ keys/edit.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "../library/library.php";
+
+$access = new Access;
+
+$access->loggedPage();
+
+Fabric::generateHead("Página principal", [
+    "/static/edit-form-api.js",
+], [
+    "/static/main-logged-in.css",
+    "/static/edit-form-api.css"
+]);
+
+Fabric::generateLoggedInStart($access);
+Fabric::generateEditForm('key');
+Fabric::generateFooter();
